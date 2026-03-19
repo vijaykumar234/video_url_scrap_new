29dbb3d9919146e82a01d676568a62e8e0df89ff
vijaykumar234 <97435578+vijaykumar234@users.noreply.github.com>
2026-03-19 21:04:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cheerio = require("cheerio");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // ✅ Enable CORS
 app.use(cors());
 
